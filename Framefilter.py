b6b38836cf02ef7fa34b657dab7fc61b4d3e2f8b
Lucas Seara Manoel <lucas@lucas.domain.lucas>
2019-08-11 17:05:30 -0300

    first commit

---
+++ Framefilter.py
@@ -0,0 +1,54 @@
+import cv2
+
+class Framefilter():
+	def __init__(self):
+		pass
+
+    # ===========================================================
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@staticmethod
+	def low_level_enhancements(frame_input):
+		frame_output = frame_input
+		return frame_output
+
+    # ===========================================================
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@staticmethod
+	def horizontal_edges_extraction(frame_input):
+		frame_output = frame_input
+		return frame_output
+
+    # ===========================================================
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@staticmethod
+	def signature_histogram_generation(frame_input):
+		frame_output = frame_input
+		return frame_output
+
+    # ===========================================================
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@staticmethod
+	def shift_estimation(frame_input):
+		frame_output = frame_input
+		return frame_output
+
+    # ===========================================================
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@staticmethod
+	def shift_filtering(frame_input):
+		frame_output = frame_input
+		return frame_output
+
+    # ===========================================================
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@staticmethod
+	def signature_reference_update(frame_input):
+		frame_output = frame_input
+		return frame_output	
+
+	# ===========================================================
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@staticmethod
+	def display_frame(frame_input):
+		frame_output = frame_input
+		return frame_output	
